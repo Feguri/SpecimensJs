aa70fa153f1149539b58aceecd19fe59c1081af3
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2021-08-04 11:22:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)] 
+  }
+  
+  
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+const newStrand = []
+for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase())
+}
+return newStrand
+}
+  
+  
+const pAequorFactory = (num, arr) => {
+    return {
+        specimenNum: num,
+        dna: arr,
+
+        mutate(){
+            const tryToMutate = () =>{
+                const before = this.dna;
+                this.dna[Math.floor(Math.random() * this.dna.length)] = returnRandBase();
+                const after = this.dna;
+                if(before === after){
+                    return false;
+                }
+                return true;
+            }
+            while(true){
+                const attempt = tryToMutate();
+                if (!attempt){
+                    break
+                }
+            }
+            return this.dna;
+
+        },
+
+        compareDNA(pAequor){
+            let matches = 0;
+            for(let i = 0; i < this.dna.length; i++){
+                if(this.dna[i] === pAequor.dna[i]){
+                    matches++;
+                }
+            };
+            let percentage = Math.round((matches / this.dna.length) * 100);
+            console.log(`These specimen have a ${percentage}% DNA match.`);
+        },
+
+        willLikelySurvive(){
+            let cOrG = 0;
+            for(let i = 0; i < this.dna.length; i++){
+                if(this.dna[i] === 'C' || this.dna[i] === 'G'){
+                    cOrG++;
+                }
+            };
+            let percentage = Math.round((cOrG / this.dna.length) * 100);
+            return(percentage >= 60 ? true : false);
+        }
+    }
+}
+
+let instancesList = [];
+let instancesNumbers = [];
+for(let i = 0; i < 30; i++){
+    const generateRandNum = () => {
+        let randomSpeciesNumber = Math.round(Math.random() * 1000);
+        let notInNumList = false;
+        while (!notInNumList){
+            notInNumList = true;
+            for(let i = 0; i < instancesNumbers.length; i++){
+                if (instancesNumbers[i] === randomSpeciesNumber){
+                    notInNumList = false;
+                }
+            }
+        }
+        instancesNumbers.push(randomSpeciesNumber);
+        return randomSpeciesNumber;
+    }
+
+    const instance = pAequorFactory(generateRandNum(), mockUpStrand());
+    instancesList.push(instance);
+}
+console.log(instancesList);
